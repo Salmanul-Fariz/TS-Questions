60a5ae96d71d936ff54160c815232d42eedb4c4f
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-14 22:46:29 +0530

    1st question completed

---
+++ typescript/Q1.ts
@@ -0,0 +1,6 @@
+// Accept a char input from the user and display it on the console.
+
+const scan = require("readline-sync");
+
+const letter = scan.question("enter a letter");
+console.log(letter);
